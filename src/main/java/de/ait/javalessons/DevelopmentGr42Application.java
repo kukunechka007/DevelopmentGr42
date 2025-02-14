497267a324453d273e5f4824cb3740c6f5f3ed9b
Kate_M <eamyskina@gmail.com>
2025-02-14 22:34:22 +0100

    First step of project

---
+++ src/main/java/de/ait/javalessons/DevelopmentGr42Application.java
@@ -0,0 +1,13 @@
+package de.ait.javalessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentGr42Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopmentGr42Application.class, args);
+    }
+
+}
